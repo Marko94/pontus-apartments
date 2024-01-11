bd4f12bd07f524c9437d686c6d4d781bb94ff18f
Marko Nenadovic <markon94@yahoo.com>
2024-01-11 23:06:57 +0100

    Scroll the page to top after URL change

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import ContactPage from './pages/Contact';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 import './style/App.css'
+import ScrollToTop from './components/scrollToTop';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ScrollToTop/>
         <Navbar/>
         <Box className="content-wrapper">
           <Routes>
-            <Route index path="/" element={<HomePage/>} />
-            <Route path="apartments" element={<ApartmentsPage/>} />
-            <Route path="gallery" element={<GalleryPage/>} />
-            <Route path="contact" element={<ContactPage/>} />
-            <Route path="*" element={<div/>}/>
+              <Route index path="/" element={<HomePage/>} />
+              <Route path="apartments" element={<ApartmentsPage/>} />
+              <Route path="gallery" element={<GalleryPage/>} />
+              <Route path="contact" element={<ContactPage/>} />
+              <Route path="*" element={<div/>}/>
           </Routes>
         </Box>
       </BrowserRouter>
