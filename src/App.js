05e6afa567da18182ace071e6682ca24f4ca0761
Marko Nenadovic <markon94@yahoo.com>
2024-01-14 20:30:52 +0100

    Add the theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import * as React from 'react';
 import { useEffect } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/navbar/Navbar';
@@ -15,21 +16,56 @@ function App() {
     window.history.scrollRestoration = 'manual'
   }, []);
 
+  
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#326273',
+      light: '#5C9ead',
+      dark: '#1c2a3e',
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#b3965a',
+      light: '#c2ab7a',
+      dark: '#8f7848',
+      contrastText: '#FFFFFF',
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'contained' &&
+            ownerState.color === 'primary' && {
+              backgroundColor: '--primary',
+              color: '#FFF',
+            }),
+        }),
+      },
+    },
+  },
+});
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <ScrollToTop/>
-        <Navbar/>
-        <Box className="content-wrapper">
-          <Routes>
-              <Route index path="/" element={<HomePage/>} />
-              <Route path="apartments" element={<ApartmentsPage/>} />
-              <Route path="gallery" element={<GalleryPage/>} />
-              <Route path="contact" element={<ContactPage/>} />
-              <Route path="*" element={<div/>}/>
-          </Routes>
-        </Box>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <ScrollToTop/>
+          <Navbar/>
+          <Box className="content-wrapper">
+            <Routes>
+                <Route index path="/" element={<HomePage/>} />
+                <Route path="apartments" element={<ApartmentsPage/>} />
+                <Route path="gallery" element={<GalleryPage/>} />
+                <Route path="contact" element={<ContactPage/>} />
+                <Route path="*" element={<div/>}/>
+            </Routes>
+          </Box>
+        </BrowserRouter>
+      </ThemeProvider>
+      
     </div>
   );
 }
