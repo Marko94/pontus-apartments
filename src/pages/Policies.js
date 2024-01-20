082fa35e2b02652519f42387f202e843162dc23e
Marko Nenadovic <markon94@yahoo.com>
2024-01-20 13:23:02 +0100

    Remove unnecessary section wrapper from Policies page

--- src/pages/Policies.js
+++ src/pages/Policies.js
@@ -13,65 +13,63 @@ import SectionWithBulletPoints from "../components/sections/SectionWithBulletPoi
 export default function PoliciesPage() {
   return (
     <Page>
-      <SectionWrapper>
-        <Box className='Policies-page-container' flex alignItems='center' justifyContent='center'>
-          <img src={CoverPhoto} className="Cover-image" alt="Cover"/>
-          <Grid container direction='column' spacing={0} className='Policies-content'>
-            <Grid item p={0}>
-              <SectionWithText
-                title={introTitle}
-                titleVariant='h4'
-                decoration={Squigle}
-                details={introDetails}
-                py={4}
-                maxWidth={'xl'}
-              />
-            </Grid>
-            <Grid item p={0}>
-              <SectionWithText
-                title={paymentTitle}
-                titleVariant='h5'
-                details={paymentDetails}
-                py={4}
-                maxWidth={'xl'}
-              />
-            </Grid>
-            <Grid item p={0}>
-              <SectionWithBulletPoints 
-                title={'General and parking'}
-                bulletList1={generalBulletList1}
-                bulletList2={generalBulletList2}
-                backgroundColor={'#f7f5f2'}
-                maxWidth={'xl'}
-              />
-            </Grid>
-            <Grid item p={0}>
-              <SectionWithBulletPoints 
-                title={'Beach and pool'}
-                bulletList1={poolBulletList1}
-                bulletList2={poolBulletList2}
-                maxWidth={'xl'}
-              />
-            </Grid>
-            <Grid item p={0}>
-              <SectionWithBulletPoints 
-                title={'Parking and transport'}
-                bulletList1={transportBulletList1}
-                bulletList2={transportBulletList2}
-                backgroundColor={'#f7f5f2'}
-                maxWidth={'xl'}
-              />
-            </Grid>
-            <Grid item p={0}>
-              <SectionWithBulletPoints 
-                title={'Distances to:'}
-                bulletList1={locationBulletList1}
-                maxWidth={'xl'}
-              />
-            </Grid>
+      <Box className='Policies-page-container' flex alignItems='center' justifyContent='center'>
+        <img src={CoverPhoto} className="Cover-image" alt="Cover"/>
+        <Grid container direction='column' spacing={0} className='Policies-content'>
+          <Grid item p={0}>
+            <SectionWithText
+              title={introTitle}
+              titleVariant='h4'
+              decoration={Squigle}
+              details={introDetails}
+              py={4}
+              maxWidth={'xl'}
+            />
           </Grid>
-        </Box>
-      </SectionWrapper>
+          <Grid item p={0}>
+            <SectionWithText
+              title={paymentTitle}
+              titleVariant='h5'
+              details={paymentDetails}
+              py={4}
+              maxWidth={'xl'}
+            />
+          </Grid>
+          <Grid item p={0}>
+            <SectionWithBulletPoints 
+              title={'General and parking'}
+              bulletList1={generalBulletList1}
+              bulletList2={generalBulletList2}
+              backgroundColor={'#f7f5f2'}
+              maxWidth={'xl'}
+            />
+          </Grid>
+          <Grid item p={0}>
+            <SectionWithBulletPoints 
+              title={'Beach and pool'}
+              bulletList1={poolBulletList1}
+              bulletList2={poolBulletList2}
+              maxWidth={'xl'}
+            />
+          </Grid>
+          <Grid item p={0}>
+            <SectionWithBulletPoints 
+              title={'Parking and transport'}
+              bulletList1={transportBulletList1}
+              bulletList2={transportBulletList2}
+              backgroundColor={'#f7f5f2'}
+              maxWidth={'xl'}
+            />
+          </Grid>
+          <Grid item p={0}>
+            <SectionWithBulletPoints 
+              title={'Distances to:'}
+              bulletList1={locationBulletList1}
+              maxWidth={'xl'}
+            />
+          </Grid>
+        </Grid>
+      </Box>
     </Page>
   );
 }
