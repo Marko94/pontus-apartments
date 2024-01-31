d7f2b15db7388cc85c39fdff79c304b72f9f1eba
Marko Nenadovic <markon94@yahoo.com>
2024-01-31 22:48:05 +0100

    Edit book now button to allow custom href

--- src/components/BookNowButton.js
+++ src/components/BookNowButton.js
@@ -1,8 +1,8 @@
 import { Button } from '@mui/material';
 import * as React from 'react';
 
-export default function BookNowButton({style}) {
+export default function BookNowButton({style, href}) {
   return (
-    <Button variant='contained' color="secondary" size='large' href='/contact' disableElevation style={style} >{'Book now'}</Button>
+    <Button variant='contained' color="secondary" size='large' href={href || '/contact'} disableElevation style={style} >{'Book now'}</Button>
   );
 }
