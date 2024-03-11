fac40b9c5a95c670f96bc819a581a47ee1673f7c
Marko Nenadovic <markon94@yahoo.com>
2024-03-11 20:44:56 +0100

    Add logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,9 @@
-import { Grid, Hidden } from '@mui/material';
+import { Box, Grid, Hidden } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { useState } from 'react';
+import logo from '../../assets/decorations/Pontius Logo - Small.png';
 import '../../style/components/Navbar.css';
 import PhoneInfoPanel from '../PhoneInfoPanel';
 import HamburgerMenu from './HamburgerMenu';
@@ -22,9 +22,9 @@ export default function Navbar() {
               </Grid>
             </Hidden>
             <Grid item xs={9} lg={6} xl={8} display="flex" alignItems="center" sx={{ flexGrow: 1, justifyContent: {xs: 'left', lg: 'center'} }}>
-              <Typography variant="h4" component="div" sx={{fontSize: {xs: "20px", sm: "24px", md: "34px"}}}>
-                Pontus Luxury Apartments
-              </Typography>
+              <Box bgcolor={isScrolled ? "#1c2a3eF6" : 'transparent'} borderRadius={50} height='64px' width='64px' display='flex' sx={{transition: '0.5s background-color ease'}}>
+                <img src={logo} alt='logo' sx={{objectFit: 'contain'}}/>
+              </Box>
             </Grid>
             <Grid item xs={3} xl={2} display="flex" alignItems="center" justifyContent="end">
               <HamburgerMenu backgroundColor={isScrolled ? 'none' : 'secondary.main'}/>
